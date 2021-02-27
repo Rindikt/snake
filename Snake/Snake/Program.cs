9b3c86b3adaa800250ff8c501671b912156d7a15
Даниил <morgot92@mail.ru>
2021-02-27 22:58:41 +0300

    fun

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -6,8 +6,22 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Point p1 = new Point();
+            p1.x = 1;
+            p1.y = 3;
+            p1.sym = '*';
+
+            p1.Drow();
+
+            Point p2 = new Point();
+            p2.x = 4;
+            p2.y = 5;
+            p2.sym = '#';
+
+            p2.Drow();
+
             Console.ReadLine();
         }
+
     }
 }
